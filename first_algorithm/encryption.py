6f47121907a4ade06c6aa3f36920fd5a300f2ce0
TimotheeGOIN <timothee.gm@gmail.com>
2026-04-01 00:05:33 +0200

    Separated the actual start of the first algorithm and the future 2nd

--- first_algorithm/encryption.py
+++ first_algorithm/encryption.py
@@ -1,7 +1,7 @@
 
 from PIL import Image
 
-image_file_path = "C:/Users/timot/Python Projects/Steganography/images/image_1.png"
+image_file_path = "/first_algorithm/images/image_1.png"
 
 base_image = Image.open(image_file_path).convert("RGBA")
 
